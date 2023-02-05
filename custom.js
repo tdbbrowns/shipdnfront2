c4d589f5dd6cb9448494cd19569de658224393b0
tdbbrowns <tdbbrowns@gmail.com>
2023-02-05 16:02:27 +0700

    first commit

---
+++ custom.js
@@ -0,0 +1,23 @@
+const notificarBtn = document.querySelector('#notificar');
+
+notificarBtn.addEventListener('click', () => {
+    Notification.requestPermission().then(resultado => {
+        console.log('Respuesta: ', resultado);
+    })
+})
+
+const verNotificacion = document.querySelector('#vernotificacion');
+
+verNotificacion.addEventListener('click', () => {
+    console.log("ss");
+    if (Notification.permission === 'granted') {
+        const notificacion = new Notification('Esta es la notificacion', {
+            icon: '/images/example-logo.jpg',
+            body: 'Tutoriales de js con blackCode'
+        });
+
+        notificacion.onclick = function(){
+            window.open('http://google.com');
+        }
+    }
+})
